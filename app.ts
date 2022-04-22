7c099ecc78c4f811dbddd25d115766e889663426
Mark Blashki <markblashki1@gmail.com>
2022-04-22 22:37:04 +1000

    add:auth, fixed eslint

--- app.ts
+++ app.ts
@@ -1,12 +1,13 @@
-import express from 'express';
+import * as express from 'express';
+import authRouter from './routes/auth';
 
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/', authRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(`Listening on port ${port}!`);
 });
+
+export default app;
